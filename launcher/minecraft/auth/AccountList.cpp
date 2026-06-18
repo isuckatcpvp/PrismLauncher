9e235b56a941d7917209bd475458a56750d2967c
isuckatcpvp <daned1947@hotmail.com>
2026-06-17 23:15:15 -0700

    crack prism
    Signed-off-by: isuckatcpvp <daned1947@hotmail.com>

--- launcher/minecraft/auth/AccountList.cpp
+++ launcher/minecraft/auth/AccountList.cpp
@@ -591,12 +591,13 @@ void AccountList::setListFilePath(QString path, bool autosave)
 
 bool AccountList::anyAccountIsValid()
 {
-    for (auto account : m_accounts) {
+    return true;
+    /*for (auto account : m_accounts) {
         if (account->ownsMinecraft()) {
             return true;
         }
     }
-    return false;
+    return false;*/
 }
 
 void AccountList::fillQueue()
